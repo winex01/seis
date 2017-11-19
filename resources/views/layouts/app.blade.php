d3134ddb89fc2ec2750ec430ad43f8e1df0d1f13
winnie damayo <winnie131212592@gmail.com>
2017-11-19 14:02:04 +0800

    add dashboard in dropdown menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,11 +70,10 @@
                         <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        {{-- <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        <li><a href="{{ route('home') }}"><i class="fa fa-dashboard" aria-hidden="true"></i>
+                            Dashboard</a>
                         </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li> --}}
+                        <li class="divider"></li>
                         <li>
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
