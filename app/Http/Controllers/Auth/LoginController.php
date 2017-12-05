c3d062d4c487f06a6183497afadf3cfc71a51619
winnie damayo <winnie131212592@gmail.com>
2017-12-06 00:29:28 +0800

    change email auth to username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // overide default username which is email
+    public function username()
+    {
+        return 'username';
+    }
 }
