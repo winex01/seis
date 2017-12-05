860982f62915982b85ad9de4a5a0881edc50d929
winnie damayo <winnie131212592@gmail.com>
2017-12-06 00:03:24 +0800

    change email column into username in users

---
+++ database/migrations/2017_12_05_155705_rename_users_column.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameUsersColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('email', 'username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('username', 'email');
+            // see user model and remove overiding user username
+        });
+    }
+}
