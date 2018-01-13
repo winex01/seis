5c1fa5f888307ab24e9919bd2f277ca715ee3a68
winnie damayo <winnie131212592@gmail.com>
2018-01-13 23:25:41 +0800

    change font awesome icon

--- resources/views/layouts/admin-sidebar2.blade.php
+++ resources/views/layouts/admin-sidebar2.blade.php
@@ -15,7 +15,7 @@
 
                     @foreach($navs as $game_id => $nav)
                         <li>
-                            <a href="{{ route('sport.show', [$game_id]) }}"><i class="fa fa-home fa-circle-thin"></i> {{ $nav }}</a>
+                            <a href="{{ route('sport.show', [$game_id]) }}"><i class="fa fa-circle-thin"></i> {{ $nav }}</a>
                         </li>
                     @endforeach
                 </ul>
