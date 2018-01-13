a1de71c9ce63ff9d90a59b7719e2e7310dc7966d
winnie damayo <winnie131212592@gmail.com>
2018-01-14 03:00:15 +0800

    add home welcome message

--- app/Match.php
+++ app/Match.php
@@ -4,9 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Matches extends Model
+class Match extends Model
 {
     //
     protected $guarded = [];
+
     
 }
--- resources/views/manager/home.blade.php
+++ resources/views/manager/home.blade.php
@@ -16,6 +16,12 @@
                         </li>
                     </ol>
                 </div>
+
+                <div class="alert alert-info">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>Message:</strong> Please choose sport's
+                </div>
+
             </div>
             <!-- /.col-lg-12 -->
         </div>
