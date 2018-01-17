e29f0cce4f4d6d05bbe268543f18e63ae933110b
winnie damayo <winnie131212592@gmail.com>
2018-01-17 22:34:53 +0800

    add column migration in games table

---
+++ database/migrations/2018_01_17_143018_create_games_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('games', function (Blueprint $table) {
+            $table->integer('medal_points')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn('medal_points');
+        });
+    }
+}
